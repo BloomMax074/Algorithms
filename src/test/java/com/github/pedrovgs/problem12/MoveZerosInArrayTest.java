5b6553c1c463577ce0f14579900e383098b28406
Hải <101235287+BloomMax074@users.noreply.github.com>
2022-06-02 14:09:46 +0700

    Update MoveZerosInArrayTest.java
    Add test case in the line 136.

--- src/test/java/com/github/pedrovgs/problem12/MoveZerosInArrayTest.java
+++ src/test/java/com/github/pedrovgs/problem12/MoveZerosInArrayTest.java
@@ -133,6 +133,14 @@ public class MoveZerosInArrayTest {
     assertZerosAtRight(array);
   }
 
+@Test public void shouldOrganizeAnArrayWithZerosPositiveAndNegativeIntegersWithTwoPointers() {
+    int[] array = { 0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8, 0, 9, 0 };
+  
+    moveZeros.moveUsingTwoPointers(array);
+  
+    assertZerosAtRight(array);
+  } 
+    
   private void assertZerosAtRight(int[] array) {
     boolean zeroFounded = false;
     for (int i : array) {
